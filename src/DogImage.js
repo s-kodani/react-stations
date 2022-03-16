5c67fa4f79cc7cef6c6df6e1ef2cb542105a483a
Shinnosuke Kodani <kocchi.jan12@gmail.com>
2022-03-16 21:21:19 +0900

    Add DogListContainer

--- src/DogImage.js
+++ src/DogImage.js
@@ -1,1 +1,9 @@
-// DO NOT DELETE
+import * as React from 'react'
+
+export function DogImage(props) {
+  return (
+    <div className="dog__content">
+      <img className="dog__img" src={props.url} />
+    </div>
+  )
+}
