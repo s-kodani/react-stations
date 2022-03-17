d8983ccc7c4ba62f2a229fe8c6f54bd9128601e5
Shinnosuke Kodani <kocchi.jan12@gmail.com>
2022-03-17 22:52:06 +0900

    station10

--- src/BreedsSelect.js
+++ src/BreedsSelect.js
@@ -1,1 +1,18 @@
-// DO NOT DELETE
+import * as React from 'react'
+
+export function BreedsSelect(props) {
+  console.log(props)
+  const options = props.breeds.map(breed => (
+    <option value={breed}>{breed}</option>
+  ))
+  return (
+    <div class="dog__content">
+      <div class="dog__select__breeds">
+        <select value={props.state} onChange={props.handleChange}>
+          {options}
+        </select>
+        <div class="select__arrow"></div>
+      </div>
+    </div>
+  )
+}
--- src/DogListContainer.js
+++ src/DogListContainer.js
@@ -1,14 +1,16 @@
 import * as React from 'react'
+import { BreedsSelect } from './BreedsSelect.js'
 
 export function DogListContainer() {
   const [breeds, setBreeds] = React.useState([])
+  const [selectedBreed, setSelectedBreed] = React.useState('')
 
   React.useEffect(() => {
     fetch('https://dog.ceo/api/breeds/list/all')
       .then(res => res.json())
       .then(
         result => {
-            setBreeds(Object.keys(result.message))
+          setBreeds(Object.keys(result.message))
         },
         error => {
           console.log(error)
@@ -16,5 +18,16 @@ export function DogListContainer() {
       )
   }, [])
 
-  return breeds
+  const handleChange = (event) => {
+    console.log(event.target.value)
+    setSelectedBreed(event.target.value)
+  }
+
+  return (
+    <BreedsSelect
+      breeds={breeds}
+      state={selectedBreed}
+      handleChange={handleChange}
+    />
+  )
 }
